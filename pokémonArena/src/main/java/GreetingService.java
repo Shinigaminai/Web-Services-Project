cfcd92cf581629f290ae37cca52e80ed710b07fa
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2020-03-05 16:02:55 +0100

    example code from quarkus/getting-started

---
+++ pokémonArena/src/main/java/GreetingService.java
@@ -0,0 +1,11 @@
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        System.out.println(name);
+        return "hello " + name;
+    }
+
+}
