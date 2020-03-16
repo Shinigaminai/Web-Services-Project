671ad7c450ee83e4345745d1be95d60445953c7d
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2020-03-16 15:05:20 +0100

    list of pokemon now works .. css corrected

--- pokémonArena/src/main/resources/META-INF/resources/pokemon-arena/javascript/pokemonDatabase.js
+++ pokémonArena/src/main/resources/META-INF/resources/pokemon-arena/javascript/pokemonDatabase.js
@@ -1,11 +1,36 @@
-var root = "pokeData";
+var root = "pokedata";
 var element = $("#pokemon-list");
 
 var loadAllPokemon = function() {
-    //TODO ask for Pokedex
-    //TODO filter pokemon
-    //TODO for each pokemon create entry
-};
+    console.log("load pokemon list");
+    getPokedex(2, function(pokedex) {
+        fillPokemonList(pokedex.pokemonEntries);
+    });
+}
+
+var fillPokemonList = function( pokemonList ) {
+    console.log(pokemonList);
+    pokemonList.forEach ( function(value) {
+        console.log(value);
+        getPokemon(value.pokemonSpecies.id, function(pokemon) {
+            createPokemonEntry(pokemon);
+        });
+    });
+}
+
+var createPokemonEntry = function( pokemon ) {
+    var entry = document.createElement("DIV");
+    var name = document.createElement("DIV");
+    name.innerHTML = pokemon.name;
+    entry.appendChild(name);
+    entry.classList.add("entry");
+    console.log(pokemon);
+    document.getElementById("pokemon-list").appendChild(entry);
+}
+
+var getPokedex = function ( id, callback) {
+    $.get("http://" + location.host + "/" + root + "/pokedex/" + id, callback);
+}
 
 var getPokemonSpecies = function ( id, callback) {
     $.get("http://" + location.host + "/" + root + "/pokemonSpecies/" + id, callback);
