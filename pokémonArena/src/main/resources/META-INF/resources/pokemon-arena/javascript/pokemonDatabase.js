fd73dfb491068955f4732987111a4ebac8935971
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2020-03-17 16:38:37 +0100

    api connection error catch

--- pokémonArena/src/main/resources/META-INF/resources/pokemon-arena/javascript/pokemonDatabase.js
+++ pokémonArena/src/main/resources/META-INF/resources/pokemon-arena/javascript/pokemonDatabase.js
@@ -63,7 +63,13 @@ var createTypeEntry = function( type ) {
 }
 
 var getPokedex = function ( id, callback) {
-    $.get("http://" + location.host + "/" + root + "/pokedex/" + id, callback);
+    $.get("http://" + location.host + "/" + root + "/pokedex/" + id, callback)
+      .done(function() {
+        console.log( "loaded pokedex" );
+      })
+      .fail(function() {
+        alert( "failed to load pokedex" );
+      });
 }
 
 var getPokemonSpecies = function ( id, callback) {
@@ -71,5 +77,8 @@ var getPokemonSpecies = function ( id, callback) {
 }
 
 var getPokemon = function ( id, callback) {
-    $.get("http://" + location.host + "/" + root + "/pokemon/" + id, callback);
+    $.get("http://" + location.host + "/" + root + "/pokemon/" + id, callback)
+      .fail(function() {
+        alert( "failed to load pokemon" );
+      });
 }
